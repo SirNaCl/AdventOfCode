1bd1f3e6feb5dc5a7722d97aa176f7cb337670c3
Emil Gedenryd <emil.gedenryd@gmail.com>
2024-12-01 17:07:29 +0100

    2024/01

---
+++ 2024/01/solution.py
@@ -0,0 +1,83 @@
+import pathlib
+import pytest
+import os
+from aocd.models import Puzzle
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+YEAR = int(PUZZLE_DIR.parent.name)
+DAY = int(PUZZLE_DIR.name)
+
+
+#### SOLUTION ####
+def parse(puzzle_input):
+    """Parse input."""
+    c1, c2 = [], []
+    for r in puzzle_input.split("\n"):
+        c1.append(int(r.split(" ")[0]))
+        c2.append(int(r.split(" ")[-1]))
+
+    return c1, c2
+
+
+def part1(data):
+    """Solve part 1."""
+    c1, c2 = data
+    c1.sort()
+    c2.sort()
+    return sum([abs(e1 - e2) for e1, e2 in zip(c1, c2)])
+
+
+def part2(data):
+    """Solve part 2."""
+    c1, c2 = data
+    return sum([e1 * c2.count(e1) for e1 in c1])
+
+
+#### UTILITY FUNCTIONS ####
+def init():
+    with open("aoc-key", "r") as keyfile:
+        os.environ["AOC_SESSION"] = keyfile.read().strip()
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    parsed = parse(puzzle_input)
+    data = parsed if parsed is not None else puzzle_input
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+def submit(puzzle, ans_a, ans_b):
+    if ans_a is None:
+        print("No solution for part 1, skipping submission!")
+    elif puzzle.answered_a:
+        print(
+            f"Already submitted correct answer a: {puzzle.answer_a}, you tried to submit {ans_a}!"
+        )
+    else:
+        print(f"Submitting {ans_a} as answer to part 1:")
+        puzzle.answer_a = ans_a
+
+    if ans_b is None:
+        print("No solution for part 2, skipping submission!")
+    elif puzzle.answered_b:
+        print(
+            f"Already submitted correct answer b: {puzzle.answer_b}, you tried to submit {ans_b}!"
+        )
+    else:
+        print(f"Submitting {ans_b} as answer to part 2:")
+        puzzle.answer_b = ans_b
+
+
+def main():
+    init()
+    puzzle = Puzzle(YEAR, DAY)
+    assert pytest.main(PUZZLE_DIR) == 0
+    ans = solve(puzzle.input_data)
+    submit(puzzle, *ans)
+
+
+if __name__ == "__main__":
+    main()
