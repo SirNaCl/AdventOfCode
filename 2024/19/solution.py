a3744469212d3b452b79decce4e922684c0e7b87
Emil Gedenryd <emil.gedenryd@gmail.com>
2024-12-20 18:32:58 +0100

    2024/19

---
+++ 2024/19/solution.py
@@ -0,0 +1,123 @@
+import pathlib
+import pytest
+import os
+from aocd.models import Puzzle
+from os import path
+import sys
+from pprint import pprint
+import re
+from functools import cache
+
+# add common util
+PATH_ROOT = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(PATH_ROOT)
+
+from common.decorators import *
+from common.func import *
+from common.grid import *
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+YEAR = int(PUZZLE_DIR.parent.name)
+DAY = int(PUZZLE_DIR.name)
+
+
+#### SOLUTION ####
+p2 = False
+
+
+def parse(puzzle_input):
+    """Parse input."""
+    tow, des = puzzle_input.split("\n\n")
+    t = re.findall(r"\w+", tow)
+    d = des.split("\n")
+    return t, d
+
+TOWELS = []
+
+@cache
+def is_possible(design: str):
+    if len(design) == 0:
+        return 1
+
+    solutions = 0
+    for t in TOWELS:
+        if design.startswith(t):
+            solutions += is_possible(design[len(t):])
+
+    return solutions
+
+
+def part1(data):
+    """Solve part 1."""
+    global TOWELS
+    t, d = data
+    TOWELS = t 
+        
+
+    return sum([1 if is_possible(dd) > 0 else 0 for dd in d])
+
+
+def part2(data):
+    """Solve part 2."""
+    global p2
+    global TOWELS
+    p2 = True
+
+    t, d = data
+    TOWELS = t 
+    solutions = 0
+    for dd in d:
+        solutions += is_possible(dd)
+
+    return solutions
+
+#### UTILITY FUNCTIONS ####
+def init():
+    with open("aoc-key", "r") as keyfile:
+        os.environ["AOC_SESSION"] = keyfile.read().strip()
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    parsed = parse(puzzle_input)
+    data = parsed if parsed is not None else puzzle_input
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+def submit(puzzle, ans_a, ans_b):
+    if ans_a is None:
+        print("No solution for part 1, skipping submission!")
+    elif puzzle.answered_a:
+        print(
+            f"Already submitted correct answer a: {puzzle.answer_a}, you tried to submit {ans_a} "
+            + f"({'correct' if str(puzzle.answer_a) == str(ans_a) else 'incorrect'})!"
+        )
+    else:
+        print(f"Submitting {ans_a} as answer to part 1:")
+        puzzle.answer_a = ans_a
+
+    if ans_b is None:
+        print("No solution for part 2, skipping submission!")
+    elif puzzle.answered_b:
+        print(
+            f"Already submitted correct answer b: {puzzle.answer_b}, you tried to submit {ans_b} "
+            + f"({'correct' if str(puzzle.answer_b) == str(ans_b) else 'incorrect'})!"
+        )
+    else:
+        print(f"Submitting {ans_b} as answer to part 2:")
+        puzzle.answer_b = ans_b
+
+
+def main():
+    init()
+    puzzle = Puzzle(YEAR, DAY)
+    assert pytest.main(PUZZLE_DIR) == 0
+    ans = solve(puzzle.input_data)
+    submit(puzzle, *ans)
+
+
+if __name__ == "__main__":
+    main()
